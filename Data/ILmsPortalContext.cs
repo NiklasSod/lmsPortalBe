b93aa21cd9029e56719a4d36aaf5db4d59360fe3
NiklasSod <nma.prob@gmail.com>
2026-08-28 03:05:54 +0200

    update to check db-token for tokenTransaction call that revokes and reissues at same time

--- Data/ILmsPortalContext.cs
+++ Data/ILmsPortalContext.cs
@@ -1,11 +1,14 @@
 ﻿using lmsPortalBe.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace lmsPortalBe.Data
 {
     public interface ILmsPortalContext
     {
         DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        DatabaseFacade Database { get; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }

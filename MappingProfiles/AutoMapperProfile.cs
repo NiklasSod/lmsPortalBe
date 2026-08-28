d0a4c222d0fb486d1564a939dc23a498f328272f
NiklasSod <nma.prob@gmail.com>
2026-08-28 02:44:07 +0200

    add the little bit of autoMapper use for now

---
+++ MappingProfiles/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using lmsPortalBe.DTOs.Auth;
+using lmsPortalBe.Models;
+
+namespace lmsPortalBe.MappingProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RegisterRequestDto, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
+        }
+    }
+}

be49876bf73bd2ef13894dbbeec808dddc182ea5
NiklasSod <nma.prob@gmail.com>
2026-08-31 08:38:34 +0200

    make mapper test easier

--- tests/lmsPortalBe.Tests/AutoMapperProfileTests.cs
+++ tests/lmsPortalBe.Tests/AutoMapperProfileTests.cs
@@ -1,7 +1,5 @@
 using AutoMapper;
-using lmsPortalBe.DTOs.Auth;
 using lmsPortalBe.MappingProfiles;
-using lmsPortalBe.Models;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace lmsPortalBe.Tests;
@@ -9,26 +7,12 @@ namespace lmsPortalBe.Tests;
 public class AutoMapperProfileTests
 {
   [Fact]
-  public void RegisterRequestDto_MapsUserNameFromEmail()
+  public void AutoMapper_Configuration_IsValid()
   {
     var configuration = new MapperConfiguration(
         cfg => cfg.AddProfile<AutoMapperProfile>(),
         NullLoggerFactory.Instance);
-    var mapper = configuration.CreateMapper();
 
-    var dto = new RegisterRequestDto
-    {
-      FirstName = "John",
-      LastName = "Smith",
-      Email = "john.smith@example.com",
-      Password = "Passw0rd1"
-    };
-
-    var user = mapper.Map<ApplicationUser>(dto);
-
-    Assert.Equal(dto.Email, user.UserName);
-    Assert.Equal(dto.FirstName, user.FirstName);
-    Assert.Equal(dto.LastName, user.LastName);
-    Assert.Null(user.PasswordHash);
+    configuration.AssertConfigurationIsValid();
   }
 }

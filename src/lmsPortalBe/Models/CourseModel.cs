89654a4f07587c45c6553bf160e1860b6e6a442a
NiklasSod <nma.prob@gmail.com>
2026-09-01 08:43:26 +0200

    add models for courses

--- src/lmsPortalBe/Models/CourseModel.cs
+++ src/lmsPortalBe/Models/CourseModel.cs
@@ -7,6 +7,6 @@ namespace lmsPortalBe.Models
         public string Description { get; set; } = string.Empty;
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; } = DateTime.Now;
-        public IEnumerable<ApplicationUser> Users { get; set; } = [];
+        public ICollection<CourseEnrollment> Enrollments { get; set; } = [];
     }
 }

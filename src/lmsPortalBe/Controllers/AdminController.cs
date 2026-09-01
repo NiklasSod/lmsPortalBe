5030b7cb8794e0ec70c41d9f125795569658e9dc
NiklasSod <nma.prob@gmail.com>
2026-09-01 08:51:27 +0200

    add route admin can delete course

--- src/lmsPortalBe/Controllers/AdminController.cs
+++ src/lmsPortalBe/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using lmsPortalBe.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace lmsPortalBe.Controllers
 {
@@ -57,6 +58,21 @@ namespace lmsPortalBe.Controllers
       }
 
       await transaction.CommitAsync();
+
+      return NoContent();
+    }
+
+    [HttpDelete("courses/{id:int}")]
+    public async Task<IActionResult> DeleteCourse(int id)
+    {
+      var course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+      if (course is null)
+      {
+        return NotFound();
+      }
+
+      _context.Courses.Remove(course);
+      await _context.SaveChangesAsync();
 
       return NoContent();
     }

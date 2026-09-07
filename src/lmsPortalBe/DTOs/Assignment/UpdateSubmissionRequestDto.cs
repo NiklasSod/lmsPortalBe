e9bdbd1285cac96e4c74a77a3198b63b470d89b0
NiklasSod <nma.prob@gmail.com>
2026-09-07 18:13:36 +0200

    add a route for current not done assignments

--- src/lmsPortalBe/DTOs/Assignment/UpdateSubmissionRequestDto.cs
+++ src/lmsPortalBe/DTOs/Assignment/UpdateSubmissionRequestDto.cs
@@ -7,6 +7,8 @@ public class UpdateSubmissionRequestDto
   public string? StudentId { get; set; } = string.Empty;
   public string? Content { get; set; }
   public string? Feedback { get; set; }
+  // TODO if SubMission is created by teacher and given to all students
+  // together with Assignment.., HandInDate should not be set at creation
   public DateTime? HandinDate { get; set; }
   public string? Status { get; set; }
 }

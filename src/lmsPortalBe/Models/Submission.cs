c16f8908b7a95c0ed088bcf92f05190acbf0b6aa
NiklasSod <nma.prob@gmail.com>
2026-09-07 18:05:21 +0200

    change StudentId from int to string

--- src/lmsPortalBe/Models/Submission.cs
+++ src/lmsPortalBe/Models/Submission.cs
@@ -6,8 +6,8 @@ namespace lmsPortalBe.Models
         public int AssignmentId { get; set; } = 0;
         // TODO add this with SubMissions 
         // public Assignment Assignment { get; set; } = null!;
-        // TODO - check - Student id is String (?)
-        public int StudentId { get; set; } = 0;
+        // public ApplicationUser Student { get; set; } = null!;
+        public string StudentId { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public string Feedback { get; set; } = string.Empty;
         public DateTime HandinDate { get; set; } = DateTime.UtcNow;

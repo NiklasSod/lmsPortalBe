e2923b012ca3d57a5a9028fa2c895a6bafe371c7
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-09-07 14:10:23 +0200

    add sudmission model

---
+++ src/lmsPortalBe/Models/Submission.cs
@@ -0,0 +1,13 @@
+namespace lmsPortalBe.Models
+{
+    public class Submission
+    {
+        public int Id { get; init; } = 0;
+        public int AssignmentId { get; set; } = 0;
+        public int StudentId { get; set; } = 0;
+        public string Content { get; set; } = string.Empty;
+        public string Feedback { get; set; } = string.Empty;
+        public DateTime HandinDate { get; set; } = DateTime.UtcNow;
+        public AssignmentStatus Status { get; set; }  = AssignmentStatus.Unsent;
+    }
+}

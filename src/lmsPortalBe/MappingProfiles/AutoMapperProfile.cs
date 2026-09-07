b10d4f95ddb8e40bad28657f18696f64d8194918
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-09-07 16:18:17 +0200

    add automapper for submissions

--- src/lmsPortalBe/MappingProfiles/AutoMapperProfile.cs
+++ src/lmsPortalBe/MappingProfiles/AutoMapperProfile.cs
@@ -22,6 +22,8 @@ namespace lmsPortalBe.MappingProfiles
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.ActivityType.ToString()));
             CreateMap<Assignment, AssignmentDto>();
+            CreateMap<Submission, SubmissionDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
         }
     }
 }

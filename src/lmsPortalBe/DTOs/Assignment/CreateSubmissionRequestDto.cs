153ef343ab8cb15f327497cac6e30d0c9098e738
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-09-09 09:36:58 +0200

    make feedback nullable in create submission dto

--- src/lmsPortalBe/DTOs/Assignment/CreateSubmissionRequestDto.cs
+++ src/lmsPortalBe/DTOs/Assignment/CreateSubmissionRequestDto.cs
@@ -12,7 +12,7 @@ public class CreateSubmissionRequestDto
   [Required]
   public string Content { get; set; } = string.Empty;
 
-  public string? Feedback { get; set; } = string.Empty;
+  public string? Feedback { get; set; }
 
   public DateTime? HandinDate { get; set; } = DateTime.UtcNow;
 }

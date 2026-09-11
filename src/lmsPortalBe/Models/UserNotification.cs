dced6cd37676001d9b8ef2bef939c5f00686eedd
NiklasSod <nma.prob@gmail.com>
2026-09-11 10:08:41 +0200

    lint

--- src/lmsPortalBe/Models/UserNotification.cs
+++ src/lmsPortalBe/Models/UserNotification.cs
@@ -1,14 +1,14 @@
 namespace lmsPortalBe.Models
 {
-    public class UserNotification
-    {
-        public int Id { get; init; } = 0;
-        public string UserId { get; set; } = string.Empty;
-        public int NotificationId { get; set; }
-        public bool IsSeen { get; set; }
-        public DateTime? SeenAt { get; set; }
+  public class UserNotification
+  {
+    public int Id { get; init; } = 0;
+    public string UserId { get; set; } = string.Empty;
+    public int NotificationId { get; set; }
+    public bool IsSeen { get; set; }
+    public DateTime? SeenAt { get; set; }
 
-        public ApplicationUser User { get; set; } = null!;
-        public Notification Notification { get; set; } = null!;
-    }
+    public ApplicationUser User { get; set; } = null!;
+    public Notification Notification { get; set; } = null!;
+  }
 }
